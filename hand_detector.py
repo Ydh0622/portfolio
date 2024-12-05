986f02b2e0eb55a7243a99f500588f2862068529
유덕현 <50919439+Ydh0622@users.noreply.github.com>
2024-12-05 14:02:49 +0900

    Add files via upload

---
+++ hand_detector.py
@@ -0,0 +1,111 @@
+import math
+import cv2
+import mediapipe as mp
+
+class handDetector():
+    def __init__(self, mode=False, maxHands=2, modelComplexity=1, detectionCon=0.5, trackCon=0.5):
+        # 손 추적을 위한 초기 설정 및 변수 초기화
+        self.lmList = []  # 손 랜드마크 좌표 저장 리스트
+        self.results = None  # 손 인식 결과 저장 변수
+        self.mode = mode  # 정지 모드 설정
+        self.maxHands = maxHands  # 최대 인식 가능한 손 개수
+        self.modelComplex = modelComplexity  # 모델 복잡도 설정
+        self.detectionCon = detectionCon  # 손 인식에 대한 신뢰도 설정
+        self.trackCon = trackCon  # 손 추적 신뢰도 설정
+
+        # MediaPipe Hands 솔루션 초기화
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.modelComplex, self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+        self.tipIds = [4, 8, 12, 16, 20]
+
+    # 이미지를 BGR에서 RGB로 변환 후 손을 인식
+    def findHands(self, img, draw=True):
+        rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(rgb_img)
+
+        # 손이 인식되었을 때 랜드마크를 그려줌x``
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+
+        return img
+
+    def findPosition(self, img, handNo=0, draw=True):
+        xList = []
+        yList = []
+        bbox = []
+        self.lmList = []
+
+
+
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+
+            for id, lm in enumerate(myHand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                xList.append(cx)
+                yList.append(cy)
+                self.lmList.append([id, cx, cy])
+
+                if draw:
+                    cv2.circle(img, (cx, cy), 6, (0, 0, 255), cv2.FILLED)
+
+            xmin, xmax = min(xList), max(xList)
+            ymin, ymax = min(yList), max(yList)
+            bbox = xmin, ymin, xmax, ymax
+
+            if draw:
+                cv2.rectangle(img, (bbox[0]-20, bbox[1]-20), (bbox[2]+20, bbox[3]+20), (0, 255, 0), 2)
+
+        return self.lmList, bbox
+
+    def fingersUp(self):
+        fingers = []
+        # Thumb
+        if self.lmList[self.tipIds[0]][1] < self.lmList[self.tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+        # 4 Fingers
+        for id in range(1, 5):
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        return fingers
+
+    def findDistance(self, p1, p2, img, draw=True):
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (0, 255, 255), 3)
+            cv2.circle(img, (x1, y1), 6, (0, 255, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 6, (0, 255, 255), cv2.FILLED)
+            cv2.circle(img, (cx, cy), 6, (0, 255, 255), cv2.FILLED)
+
+        length = math.hypot(x2 - x1, y2 - y1)
+
+        return length, img, [x1, y1, x2, y2, cx, cy]
+
+    def findAngle(self, p1, p2, p3, img, draw=True):
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        x3, y3 = self.lmList[p3][1:]
+        angle = math.degrees(math.atan2(y3 - y2, x3 - x2) - math.atan2(y1 - y2, x1 - x2))
+        if angle < 0:
+            angle += 360
+
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 255, 255), 3)
+            cv2.line(img, (x3, y3), (x2, y2), (255, 255, 255), 3)
+            cv2.circle(img, (x1, y1), 15, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x1, y1), 15, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (0, 0, 255), cv2.FILLED)
+
+        return angle
